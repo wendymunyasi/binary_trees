9c1aa9933d0d4867ef22ea8594c662d5bb2c9b6d
wendy munyasi <wendymunyasi@gmail.com>
2022-12-08 13:52:31 +0300

    Add comments to code

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -10,14 +10,22 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
+	/* If the tree is empty, return 0. */
 	if (!tree)
 		return (0);
 
+	/* If the tree is a leaf node, return 1. */
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
+
+	/* If the tree has both left and right child nodes, */
 	if (tree->left != NULL && tree->right != NULL)
+
+		/* recursively call the function on both child nodes. */
 		return (binary_tree_is_complete(tree->left) &&
 			binary_tree_is_complete(tree->right));
+
+	/* If the tree has only one child node, return 0. */
 	return (0);
 }
 
