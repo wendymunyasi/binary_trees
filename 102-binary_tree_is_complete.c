da12735e0e6da34f3eb3a2219e1eb7745bf1dbfe
wendy munyasi <wendymunyasi@gmail.com>
2022-12-08 13:47:19 +0300

    Attempt to fix codefails - task 21 - try 4

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -13,12 +13,11 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left && tree->right)
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left != NULL && tree->right != NULL)
 		return (binary_tree_is_complete(tree->left) &&
 			binary_tree_is_complete(tree->right));
-
-	if (!tree->left && !tree->right)
-		return (1);
 	return (0);
 }
 
