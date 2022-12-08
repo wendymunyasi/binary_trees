28e5691949c7efd9ba2cc5daa1eb2e608d1ba6f8
wendy munyasi <wendymunyasi@gmail.com>
2022-12-08 13:34:19 +0300

    Add comments to code

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -22,3 +22,5 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 
 	return (0);
 }
+
+/* CODE DOESN'T PASS ALL CHECKS */
--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -10,31 +10,46 @@
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
+	/* If the tree is empty, it's not a BST. */
 	if (tree == NULL)
 		return (0);
+	/* If the tree has no children, it's a BST. */
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
+	/* If the tree has two children, */
 	if (tree->left != NULL && tree->right != NULL)
 	{
+		/*
+		* check that left child is less than the parent and the right
+		* child is greater than the parent,
+		*/
 		if (tree->left->n < tree->n && tree->right->n > tree->n)
+			/* If so, recursively check that left and right subtrees are also BSTs */
 			return (binary_tree_is_bst(tree->left) &&
 				binary_tree_is_bst(tree->right));
 		else
 			return (0);
 	}
+	/* If the tree has only a left child, */
 	if (tree->left != NULL)
 	{
+		/* check that the left child is less than the parent. */
 		if (tree->left->n < tree->n)
+			/* If so, recursively check that the left subtree is a BST. */
 			return (binary_tree_is_bst(tree->left));
 		else
 			return (0);
 	}
-	if (tree->right != NULL)
+	if (tree->right != NULL) /* If the tree has only a right child, */
 	{
+		/* check that the right child is greater than the parent. */
 		if (tree->right->n > tree->n)
+			/* If so, recursively check that the right subtree is a BST. */
 			return (binary_tree_is_bst(tree->right));
 		else
 			return (0);
 	}
 	return (0);
 }
+
+/* CODE DOESN'T PASS ALL CHECKS */
