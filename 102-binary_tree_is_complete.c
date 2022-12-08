8dd1a2786c9fc0fc93d022f55514ee3578f32e39
wendy munyasi <wendymunyasi@gmail.com>
2022-12-08 12:14:34 +0300

    Add task 21 - function that checks if a binary tree is complete

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_complete - checks if a binary tree is complete.
+ * @tree: pointer to the root node of the tree to check.
+ *
+ * If tree is NULL, your function must return 0.
+ *
+ * Return: 1 if complete, 0 otherwise
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -100,4 +100,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second);
 
+/* 102-binary_tree_is_complete.c */
+int binary_tree_is_complete(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
